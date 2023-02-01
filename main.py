6c01a3bad5566c395eb9f29e6428c58e72efd472
Leon Glaser <107381521+lnglaser@users.noreply.github.com>
2023-02-01 15:35:19 +0000

    Add nested if/else age check

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
 	print("You can ride the rollercoaster!")
+	age = int(input("How old are you? "))
+	if age <= 18:
+		print("Your ticket is $7")
+	else:
+		print("Your ticket is $12")
 else:
 	print("Sorry, you have to be taller before you can ride.")
   
