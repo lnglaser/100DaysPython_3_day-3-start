5421da52e36f1d47b658ff919c7b854231a8e01a
Leon Glaser <107381521+lnglaser@users.noreply.github.com>
2023-02-01 15:40:03 +0000

    Add elif to nested if/else

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
 	print("You can ride the rollercoaster!")
 	age = int(input("How old are you? "))
-	if age <= 18:
+	if age < 12:
+		print("Your ticket is $5")
+	elif age <= 18:
 		print("Your ticket is $7")
 	else:
 		print("Your ticket is $12")
