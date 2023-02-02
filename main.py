047c55e4b2245b83ecffd92afc41a3e7d59ba1e6
Leon Glaser <107381521+lnglaser@users.noreply.github.com>
2023-02-02 18:41:31 +0000

    Add elif to pricing

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ if height >= 120:
 	elif age <= 18:
 		bill = 7
 		print("Youth tickets are $7")
+	elif age >= 45 or age <= 55:
+		bill = 0
+		print("Your ticket is $0")
 	else:
 		bill = 12
 		print("Adult tickets are $12")
 
 	wants_photo = input("Would you like a photo taken? (Y/N)")
-	if wants_photo == "Y" or "y":
+	if wants_photo == "Y" or wants_photo == "y":
 		bill += 3
 
 	print(f"Your total ticket price is ${bill}")
